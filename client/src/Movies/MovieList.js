f1944b956b3a7c924f682357f7843c5753392501
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-02-21 17:08:11 -0700

    got movie to display

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import {Link, useRouteMatch} from 'react-router-dom';
 
 export default function MovieList(props) {
+  
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+          <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+  const { title, director, metascore, id} = props.movie;
+  
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+      <div className="movie-card">
+        <Link to={`/movies/${id}`}>
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+        </Link> 
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
   );
 }
